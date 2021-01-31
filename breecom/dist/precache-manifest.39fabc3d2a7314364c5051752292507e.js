425e690ccd96ca23d322df735764610d864dec55
OtecEgoriy <55103590+OtecEgoriy@users.noreply.github.com>
2021-02-01 00:09:02 +0300

    Add gallery Img and focus like

---
+++ breecom/dist/precache-manifest.39fabc3d2a7314364c5051752292507e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "209a46790c098bd24f29",
+    "url": "css/app.f0a9595b.css"
+  },
+  {
+    "revision": "91adb6b2859f1cabf653488cd5d5533d",
+    "url": "img/BgAboutModel.91adb6b2.png"
+  },
+  {
+    "revision": "a47519b97203b04ae2ca3f20138c89eb",
+    "url": "img/arrow.a47519b9.svg"
+  },
+  {
+    "revision": "c6826bcc4f4080ad345df3598c28f278",
+    "url": "img/basket.c6826bcc.svg"
+  },
+  {
+    "revision": "6bf9d78497fd90ccdedb7a7b72a5195f",
+    "url": "img/img-2.6bf9d784.png"
+  },
+  {
+    "revision": "69971b6a47f17034578da65c000d3d88",
+    "url": "img/img.69971b6a.png"
+  },
+  {
+    "revision": "0455ed2f875885afeba77dbcd446e7f4",
+    "url": "img/logo-header.0455ed2f.svg"
+  },
+  {
+    "revision": "ff37731e24f05047f1eca7bff7273fa7",
+    "url": "img/loop.ff37731e.svg"
+  },
+  {
+    "revision": "05e2f96526a0f93011bf829da682b1ae",
+    "url": "img/preview-nike-color.05e2f965.svg"
+  },
+  {
+    "revision": "72f71474831d3ccca0d7dbdd07870ba1",
+    "url": "img/preview-nike-filter.72f71474.svg"
+  },
+  {
+    "revision": "874aea056ba176fe5749eacfb4745e68",
+    "url": "img/sign-in.874aea05.svg"
+  },
+  {
+    "revision": "d4d5f49d36e7125f39f9c70cabf3b33f",
+    "url": "index.html"
+  },
+  {
+    "revision": "209a46790c098bd24f29",
+    "url": "js/app.51b87d6c.js"
+  },
+  {
+    "revision": "3fc14a190ebfc1b6fa9b",
+    "url": "js/chunk-vendors.468bc6f5.js"
+  },
+  {
+    "revision": "6a211ad7d25b66c24f73f9295373ca3a",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
